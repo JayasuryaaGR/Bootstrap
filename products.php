1edbadb3d9005779a73a83b5d6bfc0b93c66f568
Jayasuryaa G R <66047413+JayasuryaaGR@users.noreply.github.com>
2020-08-22 14:47:21 +0530

    Add files via upload

---
+++ products.php
@@ -0,0 +1,255 @@
+<html>
+
+<head>
+
+<title> Products </title>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+<script type="text/javascript" src="bootsrap/js/jquery-3.5.1.min.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+
+<style>
+
+footer{padding: 10px 0;
+background-color: #101010;
+color:#9d9d9d;
+bottom: 0;
+width: 100%;
+}
+
+</style>
+
+</head>
+
+
+
+
+<body>
+
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+<div class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<div class="navbar-brand">Lifestyle Store</div>
+</div>
+<div class="collapse navbar-collapse" id="mynavbar">
+<ul class="nav navbar-nav navbar-right">
+<li class="active" style="padding-right:5px;"><a href="cart.html">
+<span class="glyphicon glyphicon-shopping-cart"></span> Cart </a></li>
+<li class="active" style="padding-right:5px;"><a href="settings.html">
+<span class="glyphicon glyphicon-user"></span> Settings</a></li>
+<li class="active" style="padding-right:5px;"><a href="logout.html">
+<span class="glyphicon glyphicon-log-out"></span> Logout </a></li>
+</ul>
+</div>
+</div>
+</nav>
+
+
+
+<div style="padding-top:100px;">
+<div class="container">
+<div class="jumbotron">
+<center>
+<h1> Welcome to our Lifestyle Store! </h1>
+<p> We have the best cameras, watches and shirts for you. No need to hunt around, we
+have all in one place </p>
+</center>
+</div>
+</div>
+</div>
+
+
+
+
+
+<div class="container">
+<div class="row text-center">
+
+<div class="col-md-3 col-sm-6">
+<div class="thumbnail">
+<img src="Bootstrap_Assignment/5.jpg" alt="image-5">
+<div class="caption">
+<h3> Cannon EOS </h3>
+<p> Price: Rs.36000.00 </p>
+<button class="btn btn-primary btn-block"> Add to cart </button>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="thumbnail">
+<img src="Bootstrap_Assignment/2.jpg" alt="image-2">
+<div class="caption">
+<h3> Sony DSLR </h3>
+<p> Price: Rs.40000.00 </p>
+<button class="btn btn-primary btn-block"> Add to cart </button>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="thumbnail">
+<img src="Bootstrap_Assignment/3.jpg" alt="image-3">
+<div class="caption">
+<h3> Sony DSLR </h3>
+<p> Price: Rs.50000.00 </p>
+<button class="btn btn-primary btn-block"> Add to cart </button>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="thumbnail">
+<img src="Bootstrap_Assignment/4.jpg" alt="image-4">
+<div class="caption">
+<h3> Olympus DSLR </h3>
+<p> Price: Rs.80000.00 </p>
+<button class="btn btn-primary btn-block"> Add to cart </button>
+</div>
+</div>
+</div>
+
+
+</div>
+</div>
+
+
+
+
+
+<div class="container">
+<div class="row text-center">
+
+<div class="col-md-3 col-sm-6">
+<div class="thumbnail">
+<img src="Bootstrap_Assignment/9.jpg" alt="image-9">
+<div class="caption">
+<h3> Titan Model #301 </h3>
+<p> Price: Rs.13000.00 </p>
+<button class="btn btn-primary btn-block"> Add to cart </button>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="thumbnail">
+<img src="Bootstrap_Assignment/10.jpg" alt="image-10">
+<div class="caption">
+<h3> Titan Model #201 </h3>
+<p> Price: Rs.3000.00 </p>
+<button class="btn btn-primary btn-block"> Add to cart </button>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="thumbnail">
+<img src="Bootstrap_Assignment/11.jpg" alt="image-11">
+<div class="caption">
+<h3> HMT <br> Milan </h3>
+<p> Price: Rs.8000.00 </p>
+<button class="btn btn-primary btn-block"> Add to cart </button>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="thumbnail">
+<img src="Bootstrap_Assignment/12.jpg" alt="image-12">
+<div class="caption">
+<h3> Faber Luba <br> #111 </h3>
+<p> Price: Rs.18000.00 </p>
+<button class="btn btn-primary btn-block"> Add to cart </button>
+</div>
+</div>
+</div>
+
+
+</div>
+</div>
+
+
+
+<div class="container">
+<div class="row text-center">
+
+<div class="col-md-3 col-sm-6">
+<div class="thumbnail">
+<img src="Bootstrap_Assignment/8.jpg" alt="image-8">
+<div class="caption"> <br>
+<h3> H&W </h3> 
+<p> Price: Rs.800.00 </p>
+<button class="btn btn-primary btn-block"> Add to cart </button>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="thumbnail">
+<img src="Bootstrap_Assignment/6.jpg" alt="image-6">
+<div class="caption">
+<h3> Luis Phil </h3>
+<p> Price: Rs.1000.00 </p>
+<button class="btn btn-primary btn-block"> Add to cart </button>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="thumbnail">
+<img src="Bootstrap_Assignment/13.jpg" alt="image-13">
+<div class="caption">
+<h3> John Zok </h3>
+<p> Price: Rs.1500.00 </p>
+<button class="btn btn-primary btn-block"> Add to cart </button>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="thumbnail">
+<img src="Bootstrap_Assignment/14.jpg" alt="image-14">
+<div class="caption">
+<h3> Jhalsani </h3>
+<p> Price: Rs.1300.00 </p>
+<button class="btn btn-primary btn-block"> Add to cart </button>
+</div>
+</div>
+</div>
+
+
+</div>
+</div>
+
+
+
+
+<footer>
+<div class=“container”>
+<center>Copyright © Lifestyle Store. All Rights Reserved | Contact Us: +91 90000 00000</center>
+</div>
+</footer>
+
+
+
+
+
+
+</body>
+
+</html>
